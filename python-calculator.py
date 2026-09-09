488744e17bdd1a0a7851b518a82ad126e2a50a34
thenmozhimahendran777-python <thenmozhimahendran777@gmail.com>
2026-09-09 07:15:51 +0530

    Create python-calculator.py

---
+++ python-calculator.py
@@ -0,0 +1,16 @@
+#python calculator 
+
+operator=input("enter the operator(+,-,*,/,%):")
+a=int(input())
+b=int(input())
+if(operator==+):
+  print(a+b)
+elif(operator==-):
+  print(a-b)
+elif(operator==*):
+  print(a*b)
+elif(operator==/):
+  print(a/b)
+elif(operator==%):
+  print(a%b)
+  
